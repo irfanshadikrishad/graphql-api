10c73384de417ac5262a9c3d4b43d93727040a96
Irfan Shadik Rishad <irfanshadikofficial@gmail.com>
2023-09-20 23:07:55 +0600

    initialized graphql

---
+++ index.js
@@ -0,0 +1,27 @@
+import chalk from "chalk";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    reviews: () => db.reviews,
+    authors: () => db.authors,
+    review(parent, args) {
+      return db.reviews.find((user) => user.id === args.id);
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(chalk.cyan(`[listen] 4000`));
